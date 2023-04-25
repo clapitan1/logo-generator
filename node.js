421c139b26ecc6cda0f5eba35d4a94ca34e8e921
clap <cilapitan1@gmail.com>
2023-04-24 19:54:40 -0700

    packages added

--- node.js
+++ node.js
@@ -1,3 +1,8 @@
 const fs = require('fs');
 const svgwrite = require('svgwrite');
 
+const shapes = ['circle', 'triangle', 'square'];
+
+function generateLogo(text, textColor, shape, shapeColor) {
+    
+}
